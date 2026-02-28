da39100880e197585d679f5e2a63c15190232ab2
arib-alam <137571897+arib-alam@users.noreply.github.com>
2026-02-28 02:34:23 -0600

    removed deprecation error
    Signed-off-by: arib-alam <137571897+arib-alam@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 
 import { joinVoiceChannel } from "@discordjs/voice";
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, Events, GatewayIntentBits } from "discord.js";
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
@@ -10,7 +10,7 @@ const client = new Client({
 const GUILD_ID = "1215833052421296188";
 const CHANNEL_ID = "1476093600600166451";
 
-client.once("ready", async () => {
+client.once(Events.ClientReady, async () => {
   const guild = await client.guilds.fetch(GUILD_ID);
   const channel = await guild.channels.fetch(CHANNEL_ID);
 
